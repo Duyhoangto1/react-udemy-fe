5001d5121f738a01d5d22e5e8200191b59e82b41
duyhoangto <hoangtdse170030@fpt.edu.vn>
2025-06-24 10:29:41 +0700

    fix errror

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
